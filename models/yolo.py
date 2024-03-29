cc1a7702e2e50fcbda5078c199f2e10f5f573220
Lakshmy <121610033+LakshmySanthosh@users.noreply.github.com>
2024-03-29 17:33:42 +0530

    added depthwiseSeparableConv
    Signed-off-by: Lakshmy <121610033+LakshmySanthosh@users.noreply.github.com>

--- models/yolo.py
+++ models/yolo.py
@@ -42,6 +42,7 @@ from models.common import (
     CrossConv,
     DetectMultiBackend,
     DWConv,
+    DepthwiseSeparableConv,
     DWConvTranspose2d,
     Expand,
     Focus,
@@ -394,6 +395,7 @@ def parse_model(d, ch):
         n = n_ = max(round(n * gd), 1) if n > 1 else n  # depth gain
         if m in {
             Conv,
+            DepthwiseSeparableConv,
             GhostConv,
             Bottleneck,
             GhostBottleneck,
